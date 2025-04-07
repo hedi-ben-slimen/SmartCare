c1d6b7ecf3a68d3ccf627ea219cf70fc98eb7be8
hedi-ben-slimen <mohamedelhedi.benslimene@gmail.com>
2025-04-07 14:45:12 +0100

    Welcome page website

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// Button click handlers
+function handleLoginClick() {
+  console.log("Login clicked");
+  // Add login logic here
+}
+
+function handleRegisterNavClick() {
+  console.log("Register nav button clicked");
+  // Add registration navigation logic here
+}
+
+function handleRegisterClick() {
+  console.log("Register Now clicked");
+  // Add registration logic here
+}
+
+// You can add more JavaScript functionality as needed
